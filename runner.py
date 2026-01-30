2e80498223e54406d2b08d15773b237cb8d6dcae
AusmitM <ausmitm715@gmail.com>
2026-01-29 18:41:07 -0600

    training code for t5-small original msmarco

--- runner.py
+++ runner.py
@@ -6,8 +6,8 @@ from vec2text.run_args import DataArguments, ModelArguments, TrainingArguments
 
 
 def main():
-    # exp = "inverter"
-    exp = "corrector"
+    exp = "inverter"
+    # exp = "corrector"
 
     parser = transformers.HfArgumentParser(
         (ModelArguments, DataArguments, TrainingArguments)
@@ -16,7 +16,7 @@ def main():
 
     # Model Arguments
     model_args.max_seq_length = 77
-    model_args.model_name_or_path = "google-t5/t5-base" #changed
+    model_args.model_name_or_path = "google-t5/t5-small" #changed
     model_args.embedder_model_name = "openai/clip-vit-large-patch14" #changed
     # model_args.embedder_dim = 1024  # CLIP-H embedder dim JUST FOR CLIP-ViT-H-14-laion2B-s32B-b79K
     model_args.num_repeat_tokens = 16
@@ -35,18 +35,18 @@ def main():
     training_args.warmup_steps = 10000
     training_args.bf16 = True
     training_args.use_wandb = True
-    # training_args.experiment = "inversion"
-    training_args.experiment = "corrector"
+    training_args.experiment = "inversion"
+    # training_args.experiment = "corrector"
     training_args.lr_scheduler_type = "constant_with_warmup"
     training_args.exp_group_name = ""
     training_args.learning_rate = 0.001
-    training_args.output_dir = "./saves/SDV1_4_t5_base"  # ./saves/{model_name}
+    training_args.output_dir = "./saves/SDV1_4_t5_small_originalmsmarco"  # ./saves/{model_name}
     training_args.save_steps = 2000
 
     if exp == "corrector":
         # Training Arguments
-        training_args.output_dir = "./saves/SDV1_4_t5_base_corrector"  # ./saves/{model_name}_corrector
-        training_args.corrector_model_alias = "t5-base_1___CLIP_ViT_L_14__msmarco__msl77__10epoch"
+        training_args.output_dir = "./saves/SDV1_4_t5_small_corrector_originalmsmarco"  # ./saves/{model_name}_corrector
+        training_args.corrector_model_alias = "t5-small___CLIP_ViT_L_14__msmarco_original__msl77__10epoch"
 
     experiment = experiment_from_args(model_args, data_args, training_args)
     experiment.run()
--- vec2text/aliases.py
+++ vec2text/aliases.py
@@ -64,6 +64,10 @@ CHECKPOINT_FOLDERS_DICT = {
 
     "t5-base_1___CLIP_ViT_L_14__msmarco__msl77__10epoch": "/mnt/c/Projects/Research/Vec2TextTraining/vec2text/saves/SDV1_4_t5_base",
     "t5-base_1___CLIP_ViT_L_14__msmarco__msl77__10epoch_correct": "/mnt/c/Projects/Research/Vec2TextTraining/vec2text/saves/SDV1_4_t5_base_corrector",
+
+
+    "t5-small___CLIP_ViT_L_14__msmarco_original__msl77__10epoch": "/mnt/c/Projects/Research/Vec2TextTraining/vec2text/saves/SDV1_4_t5_small_originalmsmarco",
+    "t5-small___CLIP_ViT_L_14__msmarco_original__msl77__10epoch_correct": "/mnt/c/Projects/Research/Vec2TextTraining/vec2text/saves/SDV1_4_t5_small_corrector_originalmsmarco",
 }
 
 
