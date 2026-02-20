fd70ede984d07b966e4a83f2c28667bef91da961
AusmitM <ausmitm715@gmail.com>
2026-02-19 20:06:21 -0600

    fixed argument errors for runnerv2

--- runner_v2.py
+++ runner_v2.py
@@ -1,3 +1,5 @@
+import sys
+
 import torch.multiprocessing as mp
 mp.set_start_method('spawn', force=True)
 import transformers
@@ -8,25 +10,38 @@ from vec2text.run_args import DataArguments, ModelArguments, TrainingArguments
 def parse_arguments():
     parser = argparse.ArgumentParser("Inverter_Corrector_Trainer")
     # Type of training
-    parser.add_argument("--experiment", type=str, default="inverter", choices=["inverter", "corrector"], help="Type of training: inverter or corrector")
+    parser.add_argument("--experiment_arg", type=str, default="inverter", choices=["inverter", "corrector"], help="Type of training: inverter or corrector")
     # Embedder model name
     # Replaced longer names for shorter ones:
     # CLIP_V1_4 = openai/clip-vit-large-patch14
     # CLIP_L_V1_4 = laion/CLIP-ViT-L-14-laion2B-s32B-b82K
     # CLIP_H_V1_4 = laion/CLIP-ViT-H-14-laion2B-s32B-b79K
-    parser.add_argument("--embedder_model_name", type=str, default="CLIP_V1_4", choices=["CLIP_V1_4", "CLIP_L_V1_4", "CLIP_H_V1_4"], help="Name of the embedder model")
+    parser.add_argument("--embedder_model_name_arg", type=str, default="CLIP_V1_4", choices=["CLIP_V1_4", "CLIP_L_V1_4", "CLIP_H_V1_4"], help="Name of the embedder model")
     # Dataset Name
-    parser.add_argument("--dataset_name", type=str, default="msmarco_100K", choices=["msmarco_100K", "msmarco_250K", "msmarco_500K", "msmarco_750K", "msmarco_1M", "msmarco_1_25M", "msmarco_1_5M", "msmarco_1_75M", "msmarco_2M"], help="Name of the dataset to use for training the inverter")
+    parser.add_argument("--dataset_name_arg", type=str, default="msmarco_100K", choices=["msmarco_100K", "msmarco_250K", "msmarco_500K", "msmarco_750K", "msmarco_1M", "msmarco_1_25M", "msmarco_1_5M", "msmarco_1_75M", "msmarco_2M"], help="Name of the dataset to use for training the inverter")
     # Output Directory
-    parser.add_argument("--output_dir", type=str, default="./saves/UnsavedModel", help="Directory to save the model and logs")
+    parser.add_argument("--output_dir_arg", type=str, default="./saves/UnsavedModel", help="Directory to save the model and logs")
     # Corrector model alias (if corrector)
-    parser.add_argument("--corrector_model_alias", type=str, help="Alias of the corrector model to use for training the inverter")
+    parser.add_argument("--corrector_model_alias_arg", type=str, help="Alias of the corrector model to use for training the inverter")
 
 
-    return parser.parse_args()
+    return parser.parse_known_args()[0]
 
 def main():
     args = parse_arguments()
+    custom_flags = ["--experiment_arg", "--embedder_model_name_arg", "--dataset_name_arg", 
+                    "--output_dir_arg", "--corrector_model_alias_arg"]
+    filtered_argv = [sys.argv[0]]
+    skip_next = False
+    for arg in sys.argv[1:]:
+        if skip_next:
+            skip_next = False
+            continue
+        if arg in custom_flags:
+            skip_next = True
+            continue
+        filtered_argv.append(arg)
+    sys.argv = filtered_argv
 
     parser = transformers.HfArgumentParser(
         (ModelArguments, DataArguments, TrainingArguments)
@@ -37,29 +52,29 @@ def main():
     model_args.max_seq_length = 77
     model_args.model_name_or_path = "google-t5/t5-small"
 
-    if args.experiment == "inverter":
+    if args.experiment_arg == "inverter":
         training_args.experiment = "inversion"
-    elif args.experiment == "corrector":
+    elif args.experiment_arg == "corrector":
         training_args.experiment = "corrector"
-        training_args.corrector_model_alias = args.corrector_model_alias
+        training_args.corrector_model_alias = args.corrector_model_alias_arg
     
 
-    if args.embedder_model_name == "CLIP_V1_4":
+    if args.embedder_model_name_arg == "CLIP_V1_4":
         model_args.embdeder_model_name = "openai/clip-vit-large-patch14"
-    elif args.embedder_model_name == "CLIP_L_V1_4":
+    elif args.embedder_model_name_arg == "CLIP_L_V1_4":
         model_args.embdeder_model_name = "laion/CLIP-ViT-L-14-laion2B-s32B-b82K"
-    elif args.embedder_model_name == "CLIP_H_V1_4":
+    elif args.embedder_model_name_arg == "CLIP_H_V1_4":
         model_args.embdeder_model_name = "laion/CLIP-ViT-H-14-laion2B-s32B-b79K"
 
     # model_args.embdeder_model_name = args.embedder_model_name
-    if args.embedder_model_name == "CLIP_H_V1_4":
+    if args.embedder_model_name_arg == "CLIP_H_V1_4":
         model_args.embedder_dim = 1024
     model_args.num_repeat_tokens = 16
     model_args.embedder_no_grad = True
     model_args.use_frozen_embeddings_as_input = True
 
     # Data Arguments
-    data_args.dataset_name = args.dataset_name
+    data_args.dataset_name = args.dataset_name_arg
     data_args.max_eval_samples = 500
 
     # Training Arguments
@@ -73,7 +88,7 @@ def main():
     training_args.lr_scheduler_type = "constant_with_warmup"
     training_args.exp_group_name = ""
     training_args.learning_rate = 0.001
-    training_args.output_dir = args.output_dir
+    training_args.output_dir = args.output_dir_arg
     training_args.save_steps = 2000
 
     experiment = experiment_from_args(model_args, data_args, training_args)
